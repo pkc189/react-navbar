c51c79ca0d15566d1379e17c3dcf0a9415db6bc6
pkc189 <pankajchauhan189@gmail.com>
2020-12-21 09:13:19 +0530

    3 files modified

--- src/Header.js
+++ src/Header.js
@@ -1,16 +1,43 @@
-import React from "react";
+import React ,{useState}from "react";
 import "./Header.css";
+
+import { VscThreeBars } from "react-icons/vsc";
+
+
+
+
 export default () => {
+const [nav,setNav] = useState(false);
+
+
+let width = (window.innerWidth);
   return (
     <>
       <header className="Header">
         <a href="/">Logo</a>
+        <p className="icon" onClick={()=>setNav(!nav)}>
+       
+          <VscThreeBars />
+        
+        </p>
+
+
+
+{  nav && width<400?
         <nav className="Nav">
           <a href="/">Home</a>
           <a href="/">Articles</a>
           <a href="/">About</a>
           <button>Logout</button>
         </nav>
+        
+        :<nav className="Nav">
+          
+        </nav>
+}
+
+
+
       </header>
     </>
   );
